438fdf153580d677cf4f089f9d5a7d6d04dee836
Arseny Yankovsky <arseny.yankovsky@emarketeer.com>
2019-05-27 09:25:53 +0200

    initial commit

--- src/main/java/com/n26/transactions/statistics/services/TransactionsService.java
+++
@@ -1,8 +0,0 @@
-package com.n26.transactions.statistics.services;
-
-import com.n26.transactions.statistics.dtos.TransactionsDto;
-
-public interface TransactionsService {
-
-    void addTransaction(TransactionsDto transaction);
-}
